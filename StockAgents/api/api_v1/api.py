cfcae8d73b6ad6086529635f4da9c81bc3391060
Abhinav Satheesh <asathee2@uci.edu>
2026-01-27 22:28:36 -0800

    fixed remaining hardcoded issues

--- StockAgents/api/api_v1/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from api.api_v1.endpoints import login, query, users
-
-api_router = APIRouter()
-api_router.include_router(login.router, tags=["login"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(query.router, tags=["analysis"])
